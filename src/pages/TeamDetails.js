20720367f8a801b5b9d6908de07ad380b8562299
iamromanh <romanh99@gmail.com>
2017-09-21 23:12:53 -0400

    teamReducer refactor and new initial state injection

--- src/pages/TeamDetails.js
+++ src/pages/TeamDetails.js
@@ -3,20 +3,16 @@ import { connect } from 'react-redux';
 
 import { getTeamProfile } from '../actions/teamAction';
 
-import { coachReducer } from '../reducers/coachReducer';
+import { teamReducer } from '../reducers/teamReducer';
 
 import Nav from '../components/Nav';
 import DashboardTeamNavigation from '../components/DashboardTeamNavigation';
 
 class TeamDetails extends Component {
   componentDidMount() {
-    const { id } = this.props.match.params
-    console.log('This is the id of the team', id);
+    const { id } = this.props.match.params;
     this.props.dispatch(getTeamProfile(id));
   }
-
-
-  // this.props.dispatch(getProfile(id))
 
 // #Genreal notes
 // Nav bar should change its state to logged in presenting the user nav options
@@ -41,7 +37,6 @@ class TeamDetails extends Component {
   // map state to props in the component
   // then pass it to the dumb component to render
 
-
   render() {
     return (
       <div>
@@ -58,11 +53,10 @@ class TeamDetails extends Component {
 }
 
 const mapStateToProps = state => ({
-  city: state.coachReducer.city,
-  first_name: state.coachReducer.first_name,
-  last_name: state.coachReducer.last_name,
-  email: state.coachReducer.email,
-  teams: state.coachReducer.team
+  city: state.teamReducer.city,
+  first_name: state.teamReducer.first_name,
+  last_name: state.teamReducer.last_name,
+  email: state.teamReducer.email,
 })
 
 export default connect(mapStateToProps)(TeamDetails)
--- src/reducers/teamReducer.js
+++ src/reducers/teamReducer.js
@@ -3,12 +3,35 @@ import { GET_TEAM_PROFILE, GET_TEAM_PROFILE_SUCCESS, GET_TEAM_PROFILE_ERROR } fr
 // create intial state
 // think about what state is needed on this page
 const initialState = {
-
+  name: '',
+  first_name: '',
+  last_name: '',
+  city: '',
+  state: '',
+  email: '',
+  players: [],
+  playerEmail: '',
+  playerPosition: '',
+  coach: [],
+  errorMessage: null
 }
 
 // create the teamReducer and set the state equal to intial state
-
+export const teamReducer = (state = initialState, action) => {
   // create the switch statement and pass in action.type
-    // writecases
-
-  // return the Object.assign on each case.
+  switch(action.type) {
+    case GET_TEAM_PROFILE_SUCCESS:
+      return Object.assign({}, state, {
+        name: action.response.data.name,
+        city: action.response.data.city,
+        state: action.response.data.state,
+      });
+      break;
+    case GET_TEAM_PROFILE_ERROR:
+      return Object.assign({}, state, {
+        errorMessage: action.err
+      })
+    default:
+      return state;
+  }
+}
