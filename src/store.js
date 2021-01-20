02aaaed5e0f0c040ec9bbf394d1e259e7e015fd7
getromandev <getroman.dev@gmail.com>
2021-01-19 23:12:16 -0500

    add form reducer to redux store

--- src/store.js
+++ src/store.js
@@ -4,18 +4,27 @@ import thunk from 'redux-thunk';
 import { loginReducer } from './reducers/loginReducer';
 import { coachReducer } from './reducers/coachReducer';
 import { teamReducer } from './reducers/teamReducer';
-import { routerReducer, reducer as formReducer} from 'redux-form';
+// import { routerReducer, reducer as formReducer} from 'redux-form';
+import { reducer as formReducer } from 'redux-form';
 
-const allReducers = combineReducers({
+// const allReducers = combineReducers({
+// 	loginReducer,
+// 	coachReducer,
+// 	teamReducer,
+// 	form: formReducer,
+// 	routing: routerReducer
+// });
+
+const rootReducer = combineReducers({
 	loginReducer,
 	coachReducer,
 	teamReducer,
-	form: formReducer,
-	routing: routerReducer
-});
+	form: formReducer
+})
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(allReducers, composeEnhancers(applyMiddleware(thunk, logger)));
+let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, logger)));
 
 export default store;
+// export default routerReducer;
