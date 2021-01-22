2a96e70b0d7c5275a3acf5431a9f87549a5076b3
getromandev <getroman.dev@gmail.com>
2021-01-21 22:31:31 -0500

    update seed data PW with proper generic PW

--- data/migrations/20170301023010_create_coaches.js
+++ data/migrations/20170301023010_create_coaches.js
@@ -1,18 +1,18 @@
 exports.up = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.createTable('coaches', function(table) {
-      table.increments('id').primary();
-      table.string('email');
-      table.string('password');
-      table.string('first_name');
-      table.string('last_name');
-      table.timestamps(true, true);
-    })
-  ]);
+	return Promise.all([
+		knex.schema.createTable('coaches', function(table) {
+			table.increments('id').primary();
+			table.string('email');
+			table.string('password');
+			table.string('first_name');
+			table.string('last_name');
+			table.timestamps(true, true);
+		})
+	]);
 };
 
 exports.down = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.dropTable('coaches')
-  ]);
+	return Promise.all([
+		knex.schema.dropTable('coaches')
+	]);
 };
--- data/seeds/seed_coaches.js
+++ data/seeds/seed_coaches.js
@@ -3,7 +3,7 @@ exports.seed = function (knex, Promise) {
 		.then(function () {
 			return knex('coaches').insert([
 				{email: 'romanh99@gmail.com', password: 'highlander', first_name: 'Isaac', last_name: 'Brewman'},
-				{email: 'hroman@theknowledgehouse.org', password: 'p@ssw0rd99', first_name: 'Danny', last_name: 'Diaz'}
+				{email: 'hroman@theknowledgehouse.org', password: 'highlander', first_name: 'Danny', last_name: 'Diaz'}
 			]);
 		});
 };
